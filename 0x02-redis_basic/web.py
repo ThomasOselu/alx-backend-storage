2a6eba4397580633e02a6b4ab9f9289917ce7354
Thomasoselu <thomasoselu1@gmail.com>
2024-05-29 13:50:34 +0000

    home directory

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,62 @@
-#!/usr/bin/env python3
-"""
-Caching request module
-"""
-import redis
 import requests
+import time
 from functools import wraps
-from typing import Callable
+
+CACHE_EXPIRATION = 10  # in seconds
+url_access_counts = {}
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
+def cache_with_expiry(expiration):
+    def decorator(func):
+        cache = {}
+
+        @wraps(func)
+        def wrapper(url):
+            # Check if the result is cached and not expired
+            if url in cache and time.time() - cache[url]['timestamp'] < expiration:
+                return cache[url]['content']
+
+            # If not cached or expired, fetch the content
+            content = func(url)
+            # Update cache
+            cache[url] = {'content': content, 'timestamp': time.time()}
+            return content
+
+        return wrapper
+
+    return decorator
+
+
+def track_url_access(func):
+    @wraps(func)
+    def wrapper(url):
+        if url in url_access_counts:
+            url_access_counts[url] += 1
+        else:
+            url_access_counts[url] = 1
+        return func(url)
+
     return wrapper
 
 
-@track_get_page
-def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
-    """
+@cache_with_expiry(CACHE_EXPIRATION)
+@track_url_access
+def get_page(url):
     response = requests.get(url)
     return response.text
+
+
+if __name__ == "__main__":
+    # Testing the get_page function
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
+    print(get_page(url))  # This will take some time to execute due to the simulated delay
+    print(f"Access count for {url}: {url_access_counts[url]}")
+
+    # Testing caching behavior
+    print(get_page(url))  # This should return immediately from cache
+    time.sleep(5)  # Sleep for less than the expiration time
+    print(get_page(url))  # This should return immediately from cache again
+
+    # Wait for cache to expire
+    time.sleep(CACHE_EXPIRATION)
+    print(get_page(url))  # This should fetch the page again due to cache expiration
