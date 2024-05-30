2810d54b5995486b17a7443c942fc60e137c571a
Thomasoselu <thomasoselu1@gmail.com>
2024-05-30 22:51:10 +0000

    cache tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 """
-Retrieve the HTML of the provided URL.
+Caching request module
 """
+import redis
 import requests
+from functools import wraps
+from typing import Callable
 
 
-def get_page(url: str) -> str:
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
     """
-    Retrieve the HTML of the provided URL.
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
 
-    :param url: The URL to retrieve the HTML from.
-    :return: The HTML of the provided URL.
+
+@track_get_page
+def get_page(url: str) -> str:
+    """ Makes a http request to a given endpoint
     """
     response = requests.get(url)
     return response.text
+
+
+if __name__ == '__main__':
+    url = 'http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.co.uk'
+    print(get_page(url))
